4726c71a160a55cc6ce059d96f07b8f0f457d4a8
ssynx <sa.belli@hotmail.it>
2017-11-02 11:05:59 +0100

    Update viperfx_intf.c

--- viperfx_intf.c
+++ viperfx_intf.c
@@ -1,27 +1,10 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
-#include <dlfcn.h>
-#include "viperfx_so.h"
+#include "viperfx_intf.h"
 
-#define ViPERFX_SO "libviperfx.so"
+/*
 #define ViPERFX_ENTRYPOINT "viperfx_create_instance"
-
-void* viperfx_load_library (const char * so_path_name)
-{
-  if (so_path_name == NULL) {
-    return dlopen (ViPERFX_SO, RTLD_NOW);
-  } else {
-    return dlopen (so_path_name, RTLD_NOW);
-  }
-}
-
-void viperfx_unload_library (void * handle)
-{
-  if (handle == NULL)
-    return;
-  dlclose (handle);
-}
 
 fn_viperfx_ep query_viperfx_entrypoint (void * handle)
 {
@@ -29,7 +12,7 @@ fn_viperfx_ep query_viperfx_entrypoint (void * handle)
     return NULL;
   return (fn_viperfx_ep)dlsym (
       handle, ViPERFX_ENTRYPOINT);
-}
+}*/
 
 int viperfx_command_set_px4_vx4x1 (viperfx_interface * intf,
 	int32_t param, int32_t value)
